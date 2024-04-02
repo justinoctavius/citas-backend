b86c3002a135efa50921926182ed6af8843ea069
justinoctavius <justinoctavio2001@gmail.com>
2024-04-01 20:50:38 -0400

    avances

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,10 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'admin',
-  database: process.env.DATABASE_NAME || 'citas',
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity{.js, .ts}'],
   migrations: ['dist/src/database/migrations/*{.js,.ts}'],
 };
